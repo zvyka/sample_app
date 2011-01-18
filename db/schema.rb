8dc85e7f5e305ae47b75c1aca2bb6340e35cde3e
Jon Indig <zimasprophet@gmail.com>
2011-01-17 21:14:25 -0500

    Added user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110112192605) do
+ActiveRecord::Schema.define(:version => 20110117230605) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110112192605) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
